4135186169e7b92b924c7fc8d524f4a7543bd8cd
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2020-10-27 17:06:13 +0100

    Fonction validate presque finie

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -28,3 +28,27 @@ function launchModal() {
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+function validate() {
+  let checkboxes = document.querySelectorAll(".test:checked");
+  let terms = document.querySelector("#checkbox1:checked");
+
+  // Check si au moins 1 ville est cochée
+  if (checkboxes.length == 0) {
+    const newDiv = document.createElement("div");
+    const newContent = document.createTextNode("Vous devez choisir une option.");
+    newDiv.appendChild(newContent);
+    document.querySelector(".cities").appendChild(newDiv);
+    return false;
+  }
+  
+  // Check si les conditions d'utilisations sont cochées
+  if (terms == null) {
+    const newDiv = document.createElement("div");
+    const newContent = document.createTextNode("Vous devez vérifier que vous acceptez les termes et conditions.");
+    newDiv.appendChild(newContent);
+    document.querySelector(".checkbox1_label").appendChild(newDiv);
+    return false;
+  }
+  alert("Merci ! Votre réservation a été reçue.");
+}
