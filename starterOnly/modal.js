e9f637af6918232e3ff59143d1cd198d82eeadbe
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2020-11-29 16:04:45 +0100

    Améliorations js + pattern email

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -16,39 +16,44 @@ const closeModalBtn = document.querySelector(".close");
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// Close modal event
-closeModalBtn.addEventListener("click", closeModal);
-
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
 // Close modal form
-function closeModal() {
+const closeModal = () => {
   modalbg.style.display = "none";
 }
 
-function validate() {
-  let checkboxes = document.querySelectorAll(".test:checked");
-  let terms = document.querySelector("#checkbox1:checked");
+// Close modal event
+closeModalBtn.addEventListener("click", closeModal);
+
+const validate = () => {
+  // Return une nodelist 
+  const checkboxes = document.querySelectorAll(".city-checkbox:checked");
+  // Return un element
+  const terms = document.querySelector("#checkbox1:checked");
+
+  const newText = document.createElement("p");
+  newText.className = "error-msg";
 
   // Check si au moins 1 ville est cochée
   if (checkboxes.length == 0) {
-    const newDiv = document.createElement("div");
-    const newContent = document.createTextNode("Vous devez choisir une option.");
-    newDiv.appendChild(newContent);
-    document.querySelector(".cities").appendChild(newDiv);
+    newText.innerHTML = "Vous devez choisir une option.";
+    document.querySelector(".cities").appendChild(newText);
     return false;
   }
   
   // Check si les conditions d'utilisations sont cochées
   if (terms == null) {
-    const newDiv = document.createElement("div");
-    const newContent = document.createTextNode("Vous devez vérifier que vous acceptez les termes et conditions.");
-    newDiv.appendChild(newContent);
-    document.querySelector(".checkbox1_label").appendChild(newDiv);
+    // Ajoute une classe
+    newText.classList.add("terms-error");
+    
+    newText.innerHTML = "Vous devez vérifier que vous acceptez les termes et conditions.";
+    document.querySelector(".checkbox1_label").appendChild(newText);
     return false;
   }
+  
   alert("Merci ! Votre réservation a été reçue.");
 }
